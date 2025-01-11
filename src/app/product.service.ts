b315616dac02c9e9637c28fa871de9e1b9a62746
Samuel Lopez <samuellopez01@hotmail.com>
2025-01-11 08:33:14 -0500

    Se crea el servicio de producto

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private urlBase = "http://localhost:8080/inventario-app/productos";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]>{
+    return this.httpClient.get<Product[]>(this.urlBase);
+  }
+}
