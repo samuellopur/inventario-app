aad174384f059bf5010f45254cc9a87972d11c7b
Samuel Lopez <samuellopez01@hotmail.com>
2025-01-14 15:29:10 -0500

    Componente add-product en Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 const routes: Routes = [
   {path: 'products', component: ProductListComponent},
-  {path: '', redirectTo: 'products', pathMatch: 'full'}
+  {path: '', redirectTo: 'products', pathMatch: 'full'},
+  {path: 'add-product', component: AddProductComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
